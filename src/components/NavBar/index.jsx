d179f78e1b4bfcd1a744ccc1fbcf0586dc91df60
User <user@Users-MacBook-Pro.local>
2023-08-14 11:39:07 +0200

    added redux to NavBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,6 @@
-import axios from "axios";
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getNavBarData } from "../../store/navbar/actions";
 import { CurrencyContext } from "../../contexts/CurrencyContext";
 import { formatNumber } from "../../utilities/formatNumber";
 import { NavBarCoinsLink } from "../NavBarCoinsLink";
@@ -28,65 +29,31 @@ import {
   StyledListSubNav,
 } from "./NavBar.styles";
 
-export const NavBar = ({ onClick, onChange, isCoins, isPortfolio }) => {
+const NavBar = ({ onClick, onChange, isCoins, isPortfolio }) => {
   const { currency } = useContext(CurrencyContext);
-  const [isLoading, setLoading] = useState(false);
-  const [btcDominance, setBtcDominance] = useState(0);
-  const [coins, setCoins] = useState(0);
-  const [ethDominance, setEthDominance] = useState(0);
-  const [marketCapChange24h, setMarketCapChange24h] = useState(0);
-  const [markets, setMarkets] = useState(0);
-  const [totalMarketCap, setTotalMarketCap] = useState("");
-  const [totalMarketCapLong, setTotalMarketCapLong] = useState("");
-  const [totalVolume, setTotalVolume] = useState("");
-  const [totalVolumeLong, setTotalVolumeLong] = useState("");
-  const [totalVolumePercentage, setTotalVolumePercentage] = useState("");
-  const [btcMarketCap, setBtcMarketCap] = useState("");
-  const [ethMarketCap, setEthMarketCap] = useState("");
+  const { navBarData } = useSelector((state) => state.navBar);
+  const dispatch = useDispatch();
 
-  const getGlobalData = async () => {
-    try {
-      const { data } = await axios("https://api.coingecko.com/api/v3/global");
-      setBtcDominance(Math.round(data.data.market_cap_percentage.btc));
-      setEthDominance(Math.round(data.data.market_cap_percentage.eth));
-      setCoins(data.data.active_cryptocurrencies);
-      setMarkets(data.data.markets);
-      setMarketCapChange24h(data.data.market_cap_change_percentage_24h_usd);
-      setTotalMarketCap(data.data.total_market_cap[currency] / 1000000000000);
-      setTotalMarketCapLong(data.data.total_market_cap[currency]);
-      setBtcMarketCap(data.data.total_market_cap["btc"]);
-      setEthMarketCap(data.data.total_market_cap["eth"]);
-      setTotalVolume(data.data.total_volume[currency] / 1000000000);
-      setTotalVolumeLong(data.data.total_volume[currency]);
-      setTotalVolumePercentage(
-        (
-          (data.data.total_volume[currency] /
-            data.data.total_market_cap[currency]) *
-          100
-        ).toFixed(2)
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const handleCurrency = (e) => {
     const newCurrency = e.target.value;
     onChange(newCurrency);
   };
   useEffect(() => {
-    getGlobalData();
+    dispatch(getNavBarData(currency));
   }, [currency]);
 
-  const totalMarketCapFormatted = formatNumber(totalMarketCap);
-  const totalMarketCapLongFormatted = formatNumber(totalMarketCapLong);
-  const btcMarketCapFormatted = formatNumber(btcMarketCap);
-  const ethMarketCapFormatted = formatNumber(ethMarketCap);
-  const totalVolumeFormatted = formatNumber(totalVolume);
-  const totalVolumeLongFormatted = formatNumber(totalVolumeLong);
+  const totalMarketCapFormatted = formatNumber(navBarData.totalMarketCap);
+  const totalMarketCapLongFormatted = formatNumber(
+    navBarData.totalMarketCapLong
+  );
+  const btcMarketCapFormatted = formatNumber(navBarData.btcMarketCap);
+  const ethMarketCapFormatted = formatNumber(navBarData.ethMarketCap);
+  const totalVolumeFormatted = formatNumber(navBarData.totalVolume);
+  const totalVolumeLongFormatted = formatNumber(navBarData.totalVolumeLong);
   const progressBarsData = {
-    totalVolumePercent: totalVolumePercentage,
-    btcDominancePercent: btcDominance,
-    ethDominancePercent: ethDominance,
+    totalVolumePercent: navBarData.totalVolumePercentage,
+    btcDominancePercent: navBarData.btcDominance,
+    ethDominancePercent: navBarData.ethDominance,
   };
   return (
     <>
@@ -111,21 +78,23 @@ export const NavBar = ({ onClick, onChange, isCoins, isPortfolio }) => {
             <StyledListSubNav>
               <SubNavListItem
                 title={"Coins"}
-                data1={coins}
-                data2={coins}
+                data1={navBarData.coins}
+                data2={navBarData.coins}
                 text={"Number of existing coins in the markget"}
               />
               <SubNavListItem
                 title={"Exchanges: "}
-                data1={markets}
-                data2={markets}
+                data1={navBarData.markets}
+                data2={navBarData.markets}
                 text={"Number of existing exchange markets to trade at"}
               />
               <SubNavTotalMarketCap
                 marketCap={totalMarketCapFormatted}
                 text={"T"}
-                marketCapChange24h={marketCapChange24h.toFixed(2)}
-                color={marketCapChange24h > 0 ? "#00FC2A" : "#FE1040"}
+                marketCapChange24h={navBarData.marketCapChange24h.toFixed(2)}
+                color={
+                  navBarData.marketCapChange24h > 0 ? "#00FC2A" : "#FE1040"
+                }
               />
               <SubNavVolumeVsMarketCap
                 totalVolume={totalVolumeFormatted}
@@ -161,3 +130,5 @@ export const NavBar = ({ onClick, onChange, isCoins, isPortfolio }) => {
     </>
   );
 };
+
+export default NavBar;
