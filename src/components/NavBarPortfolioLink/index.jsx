c13253cf38d661e30b574f401f4d762c41a8ef53
User <user@Users-MacBook-Pro.local>
2023-12-10 08:47:32 +0100

    removed commented out code

--- src/components/NavBarPortfolioLink/index.jsx
+++ src/components/NavBarPortfolioLink/index.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { ListItemPortfolio, StyledLink } from "./NavBarPortfolioLink.styles";
 
-// type NavBarPortfolioLinkProps = {
-//   onClick: any,
-//   background: any
-// }
-
 export const NavBarPortfolioLink = ({ onClick, background }) => {
   return (
     <ListItemPortfolio onClick={onClick} background={background}>
--- src/components/NavBarWatchlistLink/index.jsx
+++ src/components/NavBarWatchlistLink/index.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { ListItemWatchlist, StyledLink } from "./NavBarWatchlistLink.styles";
 
-// type NavBarWatchlistLinkProps = {
-//   onClick: any,
-//   background: any
-// }
-
 export const NavBarWatchlistLink = ({ onClick, background }) => {
   return (
     <ListItemWatchlist onClick={onClick} background={background}>
